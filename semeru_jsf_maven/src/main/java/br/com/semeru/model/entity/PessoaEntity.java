b223a221eb35913a150954d204dda14de94e8dd8
Dih-DesktopW10 <Dih-DesktopW10@192.168.1.101>
2016-04-21 20:30:08 -0300

    Adição dos FaceListener e a entidade Pessoa

---
+++ semeru_jsf_maven/src/main/java/br/com/semeru/model/entity/PessoaEntity.java
@@ -0,0 +1,136 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.semeru.model.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+
+/**
+ *
+ * @author Dih-DesktopW10
+ */
+@Entity
+@Table(name = "pessoa")
+public class PessoaEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue
+    @Column(name = "idPessoa", nullable = false)
+    private Integer idPessoa;
+
+    @Column(name = "nome", nullable = false, length = 80)
+    private String nome;
+
+    @Column(name = "email", length = 80)
+    private String email;
+
+    @Column(name = "telefone", length = 20)
+    private String telefone;
+
+    @Column(name = "cpf", length = 20)
+    private String cpf;
+
+    @Temporal(javax.persistence.TemporalType.DATE)
+    @Column(name = "dataDeNascimento")
+    private Date dataDeNascimento;
+
+    @Temporal(javax.persistence.TemporalType.DATE)// hora min e segundo
+    @Column(name = "dataDeCadastro")
+    private Date dataDeCadastro;
+
+    public PessoaEntity() {
+    }
+
+    public Integer getIdPessoa() {
+        return idPessoa;
+    }
+
+    public void setIdPessoa(Integer idPessoa) {
+        this.idPessoa = idPessoa;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Date getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+
+    public void setDataDeNascimento(Date dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
+    }
+
+    public Date getDataDeCadastro() {
+        return dataDeCadastro;
+    }
+
+    public void setDataDeCadastro(Date dataDeCadastro) {
+        this.dataDeCadastro = dataDeCadastro;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 43 * hash + (this.idPessoa != null ? this.idPessoa.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final PessoaEntity other = (PessoaEntity) obj;
+        if (this.idPessoa != other.idPessoa && (this.idPessoa == null || !this.idPessoa.equals(other.idPessoa))) {
+            return false;
+        }
+        return true;
+    }
+
+}
---
+++ semeru_jsf_maven/src/main/java/br/com/semeru/util/PhaseListener.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.semeru.util;
+
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author Dih-DesktopW10
+ */
+public class PhaseListener implements javax.faces.event.PhaseListener {
+
+    @Override
+    public void afterPhase(PhaseEvent phase) {//abrir sessão do hibernate e setala
+        if (phase.getPhaseId().equals(PhaseId.RESTORE_VIEW)) {
+            System.out.println("Antes da Phase: " + getPhaseId().toString());
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            FacesContextUtil.setRequestSession(session);
+
+        }
+    }
+
+    @Override
+    public void beforePhase(PhaseEvent phase) {
+        System.out.println("Depois da Phase " + getPhaseId().toString());
+        if (phase.getPhaseId().equals(PhaseId.RENDER_RESPONSE)) {
+            Session session = FacesContextUtil.getRequestSession();
+            try {
+                session.getTransaction().commit();
+            } catch (Exception e) {
+                if (session.getTransaction().isActive()) {// se der algo errado ele vai da um rollback no banco de dados
+                    session.getTransaction().rollback();
+                }
+            } finally {
+                session.close();
+            }
+        }
+
+    }
+
+    @Override
+    public PhaseId getPhaseId() {
+        return PhaseId.ANY_PHASE;
+    }
+
+}
