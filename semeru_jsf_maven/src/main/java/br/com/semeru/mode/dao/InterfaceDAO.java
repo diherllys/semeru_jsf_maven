332291eb378545276cf5679608c570b422aae5fc
Dih-DesktopW10 <Dih-DesktopW10@192.168.1.101>
2016-04-21 19:45:37 -0300

    HibernateDAO

---
+++ semeru_jsf_maven/src/main/java/br/com/semeru/mode/dao/InterfaceDAO.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.semeru.mode.dao;
+
+import java.io.Serializable;
+import java.util.List;
+import org.hibernate.criterion.DetachedCriteria;
+
+/**
+ *
+ * @author Dih-DesktopW10
+ */
+public interface InterfaceDAO<T> {
+
+    void save(T entity);
+
+    void update(T entity);
+
+    void remove(T entity);
+
+    void merge(T entity);
+
+    T getEntity(Serializable id);
+
+    T getEntityByDetacheCriteria(DetachedCriteria criteria);
+
+    List<T> getListByDetacheCriteria(DetachedCriteria criteria);
+
+    List<T> getEntities();
+
+}
