8dd2d9a3da38e33fe18519c146b0f6bfc1ff25fc
Usuario <Usuario@SANTO>
2016-04-25 09:48:13 -0300

    Adição da estrutura de diretorios web e do hibernate.cfg

--- semeru_jsf_maven/src/main/java/br/com/semeru/model/dao/HibernateDAO.java
+++ semeru_jsf_maven/src/main/java/br/com/semeru/model/dao/HibernateDAO.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.com.semeru.mode.dao;
+package br.com.semeru.model.dao;
 
 import java.io.Serializable;
 import java.util.List;
